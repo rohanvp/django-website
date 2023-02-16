556b89b00eeab15959f1b3ffd7c79721a18c0047
Rohan Paranjpe <48198937+rohanvp@users.noreply.github.com>
2023-02-16 00:47:12 -0500

    final commit

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Offer
 
 # Register your models here.
-admin.site.register(Product)
+class OfferAdmin(admin.ModelAdmin):
+    list_display=('code','discount')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('name','price','stock')
+
+
+admin.site.register(Offer,OfferAdmin)
+admin.site.register(Product,ProductAdmin)
--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello Word')
+    products=Product.objects.all()
+    return render(request,'index.html',{'products':products})
 
 def new(request):
     return HttpResponse('New Page')
